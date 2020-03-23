56eb37fde25284ca91e46b62f50c7ed38666353a
Pasindu Perera <PPerera6@gmail.com>
2020-03-23 16:23:30 +0000

    First Commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+const PORT = process.env.PORT || 8000
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(PORT, function(){
+  console.log('listening on *:'+PORT);
+});
